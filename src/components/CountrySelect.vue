cd6303ed2edea795fed2c75d0d9cb361eb8bd84a
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-07-16 09:51:59 +0200

    Create clear method

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,5 +1,6 @@
 <template>
   <select
+    @change="onChange()"
     v-model="selected"
     class="form-select mt-10 block w-full border p-3 rounded"
   >
@@ -18,6 +19,15 @@ export default {
     return {
       selected: 0,
     };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+
+      console.log(country);
+
+      this.$emit("get-country", country);
+    },
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,28 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
-    <CountrySelect :countries="countries" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Featching Data</div>
     <img :src="loadingImage" class="w-24 m-auto" alt="" />
   </main>
+
+  <button
+    @click="clearCountryData"
+    v-if="stats.Country"
+    class="
+      bg-green-700
+      text-white
+      rounded
+      p-3
+      mt-10
+      focus:outline-none
+      hover:bg-green-600
+    "
+  >
+    Clear Country
+  </button>
 </template>
 
 <script>
@@ -37,6 +53,17 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.featchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
